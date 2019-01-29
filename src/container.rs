47d24c4132b859a41ea1f52c419c5192ba4bf885
Talha Zengin <talha.zengin@matriksdata.com>
2019-01-29 17:28:59 +0300

    Container changed.

--- src/container.rs
+++ src/container.rs
@@ -10,39 +10,50 @@ pub trait Key: Debug + Clone + Ord + Any + Send + Sync
 {
 }
 
-impl<T> Key for T
-    where T: Debug + Clone + Ord + Any + Send + Sync 
-{ 
-}
-
-pub trait Service: Any + Sized 
+pub trait Service: Any 
 {
     type Key: Key;
     fn key() -> &'static Self::Key;
 }
 
+// impl<T> Key for T
+//     where T: Debug + Clone + Ord + Any + Send + Sync 
+// { 
+// }
+
 
 /**** CONTAINER. ****/
 
+pub trait GenericRegistrar<TKey, TService>
+    where TKey: Key, TService: Service
+{
+    fn register<T>(&mut self, svc: T) -> &mut Self
+        where T: Service<Key = TKey> + Into<Box<TService>>;
+}
+
 // TODO: Consider why BTreeMap ? Why not HashMap or something?
-pub struct Container<TKey: Sized, TService: Sized> {
+pub struct Container<TKey, TService> {
     services: BTreeMap<TKey, TService>,
 }
 
-impl<TKey: Sized, TService: Sized> Container<TKey, TService> 
+impl<TKey, TService> Container<TKey, TService> 
     where TKey: Key, TService: Any
 {
     pub fn new() -> Self {
         Container { services: BTreeMap::new() }
     }
 
-    pub fn register_service(&mut self, key: TKey, svc: Box<TService>) -> &mut Self {
+    pub fn register(&mut self, key: TKey, svc: Box<TService>) -> &mut Self {
         self
     }
+}
 
-    pub fn register<T>(&mut self, svc: T) -> &mut Self
+impl<TKey, TService> GenericRegistrar<TKey, TService> for Container<TKey, TService>
+        where TKey: Key, TService: Service
+{
+    fn register<T>(&mut self, svc: T) -> &mut Self
         where T: Service<Key = TKey> + Into<Box<TService>>,
     {
-        self.register_service(T::key().clone(), svc.into())
+        self.register(T::key().clone(), svc.into())
     }
 }
